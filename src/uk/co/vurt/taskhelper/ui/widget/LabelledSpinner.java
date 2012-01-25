d6bf418f449ac319b0d9e01f3cc44fa1b3ebccd8
Giles Paterson <giles@vurt.co.uk>
2012-01-25 17:13:53 +0000

    First draft at implementing multi select support in the select widget.

--- src/uk/co/vurt/taskhelper/ui/widget/LabelledSpinner.java
+++ src/uk/co/vurt/taskhelper/ui/widget/LabelledSpinner.java
@@ -1,5 +1,8 @@
 package uk.co.vurt.taskhelper.ui.widget;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uk.co.vurt.taskhelper.R;
 import uk.co.vurt.taskhelper.domain.NameValue;
 import android.content.Context;
@@ -10,19 +13,28 @@ import android.widget.TextView;
 public class LabelledSpinner extends AbstractLabelledWidget {
 
 	Spinner spinner;
+	boolean multiSelect = false;
 	
 	public LabelledSpinner(Context context, String labelText) {
+		this(context, labelText, false);
+	}
+	
+	public LabelledSpinner(Context context, String labelText, boolean multiSelect){
 		super(context);
+		this.multiSelect = multiSelect;
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		inflater.inflate(R.layout.labelled_spinner, this);
-		
 		label = (TextView)findViewById(R.id.labelled_spinner_label);
-		spinner = (Spinner)findViewById(R.id.labelled_spinner_value);
-		
 		setLabel(labelText);
+		if(multiSelect){
+			inflater.inflate(R.layout.labelled_multiselect_spinner, this);
+			spinner = (MultiSelectSpinner)findViewById(R.id.labelled_spinner_value);
+		}else{
+			inflater.inflate(R.layout.labelled_spinner, this);
+			spinner = (Spinner)findViewById(R.id.labelled_spinner_value);
+		}
 		spinner.setPrompt(labelText);
 	}
-
+	
 	public void setAdapter(NameValueAdapter arrayAdapter) {
 		spinner.setAdapter(arrayAdapter);
 	}
@@ -42,4 +54,24 @@ public class LabelledSpinner extends AbstractLabelledWidget {
 		return adapter.getNameValue(spinner.getSelectedItemPosition()).getValue();
 	}
 
+	public List<NameValue> getSelectedNameValues(){
+		if(multiSelect){
+			return ((MultiSelectSpinner)spinner).getSelectedItems();
+		}else{
+			ArrayList<NameValue> result = new ArrayList<NameValue>();
+			result.add(getSelectedNameValue());
+			return result;
+		}
+	}
+	
+	public String[] getSelectedValues(){
+		List<NameValue> selectedItems = getSelectedNameValues();
+		String[] selectedValues = new String[selectedItems.size()];
+		int i = 0;
+		for(NameValue item: selectedItems){
+			selectedValues[i] = item.getValue();
+			i++;
+		}
+		return selectedValues;
+	}
 }
