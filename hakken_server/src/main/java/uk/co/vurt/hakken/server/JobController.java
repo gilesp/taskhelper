47c41c1b83e7ae2a2418135cc12e53f41677fd4f
Giles Paterson <giles@vurt.co.uk>
2012-04-13 16:22:09 +0100

    Used HashUtils to validate job request in line with update rest_api documentation.

--- hakken_server/src/main/java/uk/co/vurt/hakken/server/JobController.java
+++ hakken_server/src/main/java/uk/co/vurt/hakken/server/JobController.java
@@ -1,7 +1,9 @@
 package uk.co.vurt.hakken.server;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import uk.co.vurt.hakken.security.HashUtils;
 import uk.co.vurt.hakken.server.model.Job;
 
 @Controller
@@ -35,9 +38,20 @@ public class JobController {
 		return jobs;
 	}
 	
-	@RequestMapping(value="for/{username}/since/{timestamp}", method=RequestMethod.GET)
-	public @ResponseBody List<Job> getJobsForUserSince(@PathVariable String username, @PathVariable String timestamp){
-		List<Job> jobs = new ArrayList<Job>();
-		return jobs;
+	@RequestMapping(value="for/{username}/{hmac}/since/{timestamp}", method=RequestMethod.GET)
+	public @ResponseBody List<Job> getJobsForUserSince(@PathVariable String username, @PathVariable String hmac, @PathVariable String timestamp){
+		Map<String, String>parameterMap = new HashMap<String, String>();
+		parameterMap.put("username", username);
+		parameterMap.put("timestamp", timestamp);
+		
+		boolean validRequest = HashUtils.validate(parameterMap, hmac);
+		if(validRequest){
+			List<Job> jobs = new ArrayList<Job>();
+			//TODO: Actually retrieve job list
+			return jobs;
+		}else {
+			//TODO: handle invalid request/send error response
+			return null;
+		}
 	}
 }
