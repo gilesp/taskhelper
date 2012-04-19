f55ac24c92b92a3f5311e1dded8619f14ea0b3b1
Giles Paterson <giles@vurt.co.uk>
2012-04-19 17:43:35 +0100

    Placeholder for TaskRegistry

---
+++ hakken-server/src/main/java/uk/co/vurt/hakken/server/task/TaskMapper.java
@@ -0,0 +1,10 @@
+package uk.co.vurt.hakken.server.task;
+
+/**
+ * Handles mapping between TaskDefinitions and DataConnectorTaskDefinitions 
+ * @author giles.paterson
+ *
+ */
+public class TaskMapper {
+
+}
