db2db3673aab71614d6697b98ad19d5d2809e2f7
Ryan Pickett <ryan@hdpe.me>
2012-04-22 15:47:32 +0100

    put an ok button on the preferences activity and send you back to the dispatcher activity when clicked

--- hakken-android-client/src/main/java/uk/co/vurt/taskhelper/activities/PreferencesActivity.java
+++ hakken-android-client/src/main/java/uk/co/vurt/taskhelper/activities/PreferencesActivity.java
@@ -4,16 +4,24 @@ import uk.co.vurt.taskhelper.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class PreferencesActivity extends PreferenceActivity {
 
+	private TextView message;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 //		boolean returnToStart = getIntent().getBooleanExtra(DispatcherActivity.RETURN_TO_START_KEY, false);
+
+		message = (TextView) findViewById(R.id.message);
 		
 		addPreferencesFromResource(R.xml.preferences);
+		setContentView(R.layout.preferences_activity);
+		
 		
 //		if(returnToStart){
 //			startActivity(new Intent(this, DispatcherActivity.class));
@@ -21,7 +29,10 @@ public class PreferencesActivity extends PreferenceActivity {
 //		}
 	}
 	
-	
+	public void handleSavePreferences(View view) {
+		startActivity(new Intent(this, DispatcherActivity.class));
+		finish();
+	}
 }
 
 
