ac9b592801f6cdc8d8c7d94184f8c2fc9899c5c0
Giles Paterson <giles@vurt.co.uk>
2012-04-24 18:30:42 +0100

    First stab at implementing expressions in tasks to determine the next page to visit.
    Currently only supports a single string expression but will be extended
    to allow for dataitem evaluation.

---
+++ hakken-android-client/src/main/java/uk/co/vurt/taskhelper/processor/csql/HakkenEvaluationVisitor.java
@@ -0,0 +1,15 @@
+package uk.co.vurt.taskhelper.processor.csql;
+
+import net.wmfs.coalesce.csql.EvaluationVisitor;
+import net.wmfs.coalesce.csql.Expression.CustomItemExpression;
+import net.wmfs.coalesce.csql.ExpressionException;
+
+public class HakkenEvaluationVisitor extends EvaluationVisitor {
+
+	@Override
+	public void visitCustomItemExpression(
+			CustomItemExpression nodeItemExpression) throws ExpressionException {
+		// TODO Auto-generated method stub
+		super.visitCustomItemExpression(nodeItemExpression);
+	}
+}
