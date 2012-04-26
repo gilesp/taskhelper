d347d83a463e00d42f67db4aea93a4e3698d2604
Giles Paterson <giles@vurt.co.uk>
2012-04-26 11:14:02 +0100

    Missed some renames inside variables

--- hakken-android-client/src/main/java/uk/co/vurt/hakken/Constants.java
+++ hakken-android-client/src/main/java/uk/co/vurt/hakken/Constants.java
@@ -2,7 +2,7 @@ package uk.co.vurt.hakken;
 
 public class Constants {
 
-	public static final String ACCOUNT_TYPE = "uk.co.vurt.taskhelper";
+	public static final String ACCOUNT_TYPE = "uk.co.vurt.hakken";
 	
-	public static final String AUTHTOKEN_TYPE = "uk.co.vurt.taskhelper";
+	public static final String AUTHTOKEN_TYPE = "uk.co.vurt.hakken";
 }
