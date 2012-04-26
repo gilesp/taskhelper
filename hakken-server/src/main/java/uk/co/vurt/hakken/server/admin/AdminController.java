9b0fa0e7cb12e5a19b48cd43bfd76c8f0c5906bf
Giles Paterson <giles@vurt.co.uk>
2012-04-26 16:11:30 +0100

    tidied up imports

--- hakken-server/src/main/java/uk/co/vurt/hakken/server/admin/AdminController.java
+++ hakken-server/src/main/java/uk/co/vurt/hakken/server/admin/AdminController.java
@@ -1,7 +1,5 @@
 package uk.co.vurt.hakken.server.admin;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
