07476d2bad44bee46ea83e53398da8d85d18025b
Giles Paterson <giles@vurt.co.uk>
2012-09-26 10:10:32 +0100

    Stub class for grid version of task definition list.

---
+++ hakken-android-client/src/main/java/uk/co/vurt/hakken/fragments/TaskDefinitionsGridFragment.java
@@ -0,0 +1,25 @@
+package uk.co.vurt.hakken.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Create a version of the TaskDefinitionsListFragment that uses a GridView
+ * 
+ * For the time being just use the same image for all task definitions, at a 
+ * later date we'll include appropriate images int eh data being synched from 
+ * the server.
+ * 
+ * Useful information about GridViews:
+ * 
+ * http://developer.android.com/guide/topics/ui/layout/gridview.html
+ * 
+ * http://stackoverflow.com/questions/10285634/how-to-gridview-inside-a-fragment
+ * 
+ * http://developer.android.com/design/building-blocks/grid-lists.html
+ * 
+ * @author giles
+ *
+ */
+public class TaskDefinitionsGridFragment extends Fragment {
+
+}
